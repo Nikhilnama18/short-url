c8d6358feee0e9ca9521cc11c7222e0a4f4b926b
Nikhil_Nama <nikhilnama1111@gmail.com>
2026-05-19 14:23:56 +0530

    Introduce postgresql layer and connect to neon db

--- src/main/java/com/nikhil/shortURL/entity/URLEntity.java
+++ src/main/java/com/nikhil/shortURL/entity/URLEntity.java
@@ -1,11 +1,15 @@
 package com.nikhil.shortURL.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "urls")
+@Getter
+@NoArgsConstructor
 public class URLEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +19,10 @@ public class URLEntity {
     @Column(nullable = false, length = 2048)
     private String longURL;
     private LocalDateTime createdAt;
+
+    public URLEntity(String alias, String longURL){
+        this.alias = alias;
+        this.longURL = longURL;
+        this.createdAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/nikhil/shortURL/repository/URLRepository.java
+++ src/main/java/com/nikhil/shortURL/repository/URLRepository.java
@@ -1,35 +1,13 @@
 package com.nikhil.shortURL.repository;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import com.nikhil.shortURL.entity.URLEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class URLRepository implements IURLRepository {
-    private static final String URL_PREFIX = "url:";
-    private final StringRedisTemplate stringRedisTemplate;
-
-    // get long url for alias
-    public Optional<String> getURLByAlias(String alias){
-        String key = URL_PREFIX + alias;
-        String longURL = stringRedisTemplate
-                .opsForValue()
-                .get(key);
-
-        return Optional.ofNullable(longURL);
-    }
-
-    // store long url if alias didn't exist
-    public boolean saveLongURLIfAbsent(String longURL, String alias){
-        String key = URL_PREFIX + alias;
-
-        Boolean saved = stringRedisTemplate
-                .opsForValue()
-                .setIfAbsent(key, longURL);
-
-        return Boolean.TRUE.equals(saved);
-    }
+public interface URLRepository extends JpaRepository<URLEntity, Long>{
+    Optional<URLEntity> findByAlias(String alias);
+    boolean existsByAlias(String alias);
 }
